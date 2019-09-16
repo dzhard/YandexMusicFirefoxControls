1c45df436a400f5ccd1729b8386733a0105e141e
Dzmitry Hardziayuk <Dzmitry.Hardziayuk@xa.epicgames.com>
2019-09-16 14:25:22 +0300

    add hot key info to the menu

--- contextMenu.js
+++ contextMenu.js
@@ -1,30 +1,54 @@
-browser.contextMenus.create({
-  id: "play-pause-menu",
-  title: 'Play/Pause',
-  contexts: ["browser_action"],
-  onclick: () => {
-    pause();
-  }
-});
-browser.contextMenus.create({
-  id: "play-next-menu",
-  title: 'Next',
-  contexts: ["browser_action"],
-  onclick: () => {
-    next()
-  }
-});
-browser.contextMenus.create({
-  id: "play-prev-menu",
-  title: 'Previous',
-  contexts: ["browser_action"],
-  onclick: () => {
-    prev()
-  }
-});
-browser.contextMenus.create({
-  id: "options-menu",
-  title: 'Options',
-  contexts: ["browser_action"],
-  onclick: () => browser.runtime.openOptionsPage()
-});
+let menuPlayName;
+let menuPrevName;
+let menuNextName;
+
+browser.commands.getAll()
+.then(res => {
+  res.forEach(sc => {
+    switch (sc.name) {
+      case'play-pause':
+        menuPlayName = `${sc.description} (${sc.shortcut})`;
+        break;
+      case'prev':
+        menuPrevName = `${sc.description} (${sc.shortcut})`;
+        break;
+      case'next':
+        menuNextName = `${sc.description} (${sc.shortcut})`;
+        break
+    }
+  });
+})
+.then(() => buildMenu());
+
+function buildMenu() {
+  browser.contextMenus.create({
+    id: "play-pause-menu",
+    title: menuPlayName,
+    contexts: ["browser_action"],
+    onclick: () => {
+      pause();
+    }
+  });
+  browser.contextMenus.create({
+    id: "play-next-menu",
+    title: menuNextName,
+    contexts: ["browser_action"],
+    onclick: () => {
+      next()
+    }
+  });
+  browser.contextMenus.create({
+    id: "play-prev-menu",
+    title: menuPrevName,
+    contexts: ["browser_action"],
+    onclick: () => {
+      prev()
+    }
+  });
+  browser.contextMenus.create({
+    id: "options-menu",
+    title: 'Options',
+    contexts: ["browser_action"],
+    onclick: () => browser.runtime.openOptionsPage()
+  });
+}
